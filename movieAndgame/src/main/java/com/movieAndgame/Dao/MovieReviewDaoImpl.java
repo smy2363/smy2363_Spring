fd5fab518d592ba3d5d533670a80866b52c9e47c
smy2363 <smy2363@naver.com>
2024-08-19 09:58:48 +0900

    0819

--- movieAndgame/src/main/java/com/movieAndgame/Dao/MovieReviewDaoImpl.java
+++ movieAndgame/src/main/java/com/movieAndgame/Dao/MovieReviewDaoImpl.java
@@ -1,5 +1,7 @@
 package com.movieAndgame.Dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.movieAndgame.Dto.MovieReviewDto;
@@ -7,4 +9,8 @@ import com.movieAndgame.Dto.MovieReviewDto;
 @Mapper
 public interface MovieReviewDaoImpl {
 	public int reviewSave(MovieReviewDto movieReviewDto);
+	
+	// 전체 데이터 가져오기 - 목록 출력
+	public List<MovieReviewDto> findAll();
+	
 }
